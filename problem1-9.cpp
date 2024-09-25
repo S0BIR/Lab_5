92b7d91cb1cae65f396816a52dae5b9cd00f48f6
S0BIR <m.musashayxov@newuu.uz>
2024-09-25 12:34:53 +0500

    problem1-9.cpp

---
+++ problem1-9.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+
+//PROBLEM1
+//int main() {
+//    for (int i = 1; i < 11; ++i) {
+//        cout<<i<<"";}
+//}
+
+
+//PROBLEM2
+//int main(){
+//    int sum=0;
+//    for (int i = 1; i < 11; ++i) {
+//        cout<<i<<" ";
+//    sum+=i;}
+//}
+
+
+//PROBLEM3
+//int main(){
+//    for (int i = 1; i <101 ; ++i) {
+//        if (i%5==0) continue;
+//        cout<<i<<" ";}
+//}
+
+
+//PROBLEM4
+//int main(){
+//    int n;
+//    cin>>n;
+//    for (int i = 0; i <= n; ++i) {
+//        if (i==0)continue;
+//        if (i%10==0)
+//            cout<<i<<" ";}
+//}
+
+
+//PROBLEM5
+//int main(){
+//    int count=0;
+//    for (char i = 'A'; i < 'Z'; ++i) {
+//        cout<<i<<" ";
+//        count++;
+//        if (count==5){
+//            cout<<endl;
+//        count=0;}
+//    }
+//}
+
+
+//PROBLEM7
+//int main(){
+//    int n;
+//    cin>>n;
+//    bool prime= true;
+//    if (n==0 || n==1){
+//        prime= false;
+//    }
+//    for (int i = 2; i <= n; ++i) {
+//        if (n%i==0){
+//            prime=false;
+//            break;}
+//        }
+//    if (prime)
+//        cout<<n<<" is a prime number";
+//    else
+//        cout<<n<<" is not a prime number";
+//}
+
+
+//PROBLEM8
+//int main(){
+//    int n;
+//    int sum=0;
+//    cin>>n;
+//    for (int i = 1; i <=n; ++i) {
+//         sum+=i*i;
+//         cout<<i<<"*"<<i<<"="<<i*i<<endl;}
+//    cout<<"Sum: "<<sum;
+//}
+
+
+//PROBLEM9
+int main(){
+    int n
+}
